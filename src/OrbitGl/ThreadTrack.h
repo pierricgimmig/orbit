04762b9175fcabeef3713ff4282f0e51efbb38c7
Pierric Gimmig <pierric@google.com>
2021-02-17 17:35:12 -0800

    Rebase and adapt to latest changes

--- src/OrbitGl/ThreadTrack.h
+++ src/OrbitGl/ThreadTrack.h
@@ -75,7 +75,7 @@ class ThreadTrack final : public TimerTrack {
   std::shared_ptr<orbit_gl::TracepointTrack> tracepoint_track_;
 
   absl::Mutex mutex_;
-  ScopeTree scope_tree_;
+  ScopeTree<TextBox> scope_tree_;
 };
 
 #endif  // ORBIT_GL_THREAD_TRACK_H_
