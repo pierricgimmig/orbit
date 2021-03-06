aa8827243522f55330139524a0479ead768cd70b
Pierric Gimmig <pierric@google.com>
2021-03-05 16:02:02 -0800

    Add GrpcFlatBuffers project

--- generate_flatbuffers.sh
+++ generate_flatbuffers.sh
@@ -3,12 +3,12 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-flatc --proto -o ./src/GrpcProtos/schemas/ ./src/GrpcProtos/services_ggp.proto --proto-namespace-suffix fbs
-flatc --proto -o ./src/GrpcProtos/schemas/ ./src/GrpcProtos/producer_side_services.proto --proto-namespace-suffix fbs
-flatc --proto -o ./src/GrpcProtos/schemas/ ./src/GrpcProtos/module.proto --proto-namespace-suffix fbs
-flatc --proto -o ./src/GrpcProtos/schemas/ ./src/GrpcProtos/services.proto --proto-namespace-suffix fbs
-flatc --proto -o ./src/GrpcProtos/schemas/ ./src/GrpcProtos/code_block.proto --proto-namespace-suffix fbs
-flatc --proto -o ./src/GrpcProtos/schemas/ ./src/GrpcProtos/capture.proto --proto-namespace-suffix fbs
-flatc --proto -o ./src/GrpcProtos/schemas/ ./src/GrpcProtos/symbol.proto --proto-namespace-suffix fbs
-flatc --proto -o ./src/GrpcProtos/schemas/ ./src/GrpcProtos/process.proto --proto-namespace-suffix fbs
-flatc --proto -o ./src/GrpcProtos/schemas/ ./src/GrpcProtos/tracepoint.proto --proto-namespace-suffix fbs
+flatc --proto -o ./src/GrpcFlatBuffers/ ./src/GrpcProtos/services_ggp.proto --proto-namespace-suffix fbs
+flatc --proto -o ./src/GrpcFlatBuffers/ ./src/GrpcProtos/producer_side_services.proto --proto-namespace-suffix fbs
+flatc --proto -o ./src/GrpcFlatBuffers/ ./src/GrpcProtos/module.proto --proto-namespace-suffix fbs
+flatc --proto -o ./src/GrpcFlatBuffers/ ./src/GrpcProtos/services.proto --proto-namespace-suffix fbs
+flatc --proto -o ./src/GrpcFlatBuffers/ ./src/GrpcProtos/code_block.proto --proto-namespace-suffix fbs
+flatc --proto -o ./src/GrpcFlatBuffers/ ./src/GrpcProtos/capture.proto --proto-namespace-suffix fbs
+flatc --proto -o ./src/GrpcFlatBuffers/ ./src/GrpcProtos/symbol.proto --proto-namespace-suffix fbs
+flatc --proto -o ./src/GrpcFlatBuffers/ ./src/GrpcProtos/process.proto --proto-namespace-suffix fbs
+flatc --proto -o ./src/GrpcFlatBuffers/ ./src/GrpcProtos/tracepoint.proto --proto-namespace-suffix fbs
---
+++ src/GrpcFlatBuffers/include/GrpcFlatBuffers/Constants.h
@@ -0,0 +1,22 @@
+// Copyright (c) 2021 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef GRPC_FLATBUFFERS_CONSTANTS_H_
+#define GRPC_FLATBUFFERS_CONSTANTS_H_
+
+#include <stdint.h>
+
+namespace orbit_grpc_flatbuffers {
+constexpr uint64_t kInvalidInternId = 0;
+constexpr uint64_t kInvalidFunctionId = 0;
+
+constexpr int kMissingInfo = -1;
+
+// Reserved producer IDs.
+constexpr uint64_t kLinuxTracingProducerId = 0;
+constexpr uint64_t kMemoryInfoProducerId = 1;
+constexpr uint64_t kExternalProducerStartingId = 1024;
+}  // namespace orbit_grpc_protos
+
+#endif  // GRPC_FLATBUFFERS_CONSTANTS_H_
