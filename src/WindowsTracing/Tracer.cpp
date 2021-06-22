e15f4696ba635ac33f87cd64e26adec49768bab4
Pierric Gimmig <pierric.gimmig@gmail.com>
2021-06-22 13:55:05 -0700

    Find file io functions

--- src/WindowsTracing/Tracer.cpp
+++ src/WindowsTracing/Tracer.cpp
@@ -11,13 +11,48 @@
 #include "WindowsTracing/EventTracer.h"
 #include "WindowsTracing/TracerListener.h"
 #include "capture.pb.h"
-#include "krabs/krabs.hpp"
+
+#include <krabs/krabs.hpp>
+#include <absl/strings/match.h>
+#include <absl/strings/ascii.h>
+#include <vector>
 
 using orbit_grpc_protos::FunctionCall;
 using orbit_grpc_protos::ThreadName;
 using orbit_grpc_protos::ThreadNamesSnapshot;
 
 namespace orbit_windows_tracing {
+
+bool IsWindowsApiDll(const std::string& file_path) {
+  static std::vector<std::string> windows_api_dlls = {"ntdll.dll", "kernel32.dll",
+                                                      "kernelbase.dll"};
+
+  std::string lower_file_path = absl::AsciiStrToLower(file_path);
+  for (const std::string& windows_api_dll : windows_api_dlls) {
+    if (absl::StrContains(lower_file_path, windows_api_dll)) return true;
+  }
+  return false;
+}
+
+bool IsFileIoFunction(const std::string& file_path, const std::string& function_name) {
+  if (!IsWindowsApiDll(file_path)) return false;
+  static std::vector<std::string> file_io_function_names = {"WriteFile", "ReadFile"};
+  for (const std::string& name : file_io_function_names) {
+    if (absl::StrContains(function_name, name)) return true;
+  }
+  return false;
+}
+
+orbit_lib::FunctionHook FunctionHookFromInstrumentedFunction(
+    const orbit_grpc_protos::InstrumentedFunction& instrumented_function) {
+  orbit_lib::FunctionHook function_hook;
+  function_hook.address = instrumented_function.function_id();
+  function_hook.type =
+      IsFileIoFunction(instrumented_function.file_path(), instrumented_function.function_name())
+          ? orbit_lib::FunctionHook::Type::kFileIO
+          : orbit_lib::FunctionHook::Type::kRegular;
+  return function_hook;
+}
 
 void Tracer::Start() {
   CHECK(event_tracer_ == nullptr);
@@ -29,18 +64,18 @@ void Tracer::Start() {
   SendModuleSnapshot();
   event_tracer_->Start();
 
-  std::vector<uint64_t> function_addresses;
+  std::vector<orbit_lib::FunctionHook> function_hooks;
   for (const orbit_grpc_protos::InstrumentedFunction& instrumented_function :
        capture_options_.instrumented_functions()) {
     LOG("Hooking function %lu", instrumented_function.function_id());
-    function_addresses.push_back(instrumented_function.function_id());
+    function_hooks.push_back(FunctionHookFromInstrumentedFunction(instrumented_function));
     // TODO-PG: Adopt function id instead of using it as address
     // instrumented_functions_.emplace_back(function_id, instrumented_function.file_path(),
     //                                      instrumented_function.file_offset());
   }
 
   capture_listener_ = std::make_unique<CaptureListener>(listener_);
-  orbit_lib::StartCapture(target_pid, function_addresses.data(), function_addresses.size(),
+  orbit_lib::StartCapture(target_pid, function_hooks.data(), function_hooks.size(),
                           capture_listener_.get());
 }
 
