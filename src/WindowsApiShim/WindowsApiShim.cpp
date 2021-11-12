65ae8181f4679f226a96054c07680e0cbda0c94a
Pierric Gimmig <pierric@google.com>
2021-11-12 15:38:02 -0800

    Add WindowsApiShim project

---
+++ src/WindowsApiShim/WindowsApiShim.cpp
@@ -0,0 +1,18 @@
+// Copyright (c) 2021 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "WindowsApiShim/WindowsApiShim.h"
+
+#include <unordered_map>
+#include <win32/manifest.h>
+#include <win32/Windows.Win32.Storage.FileSystem.h>
+
+extern "C" {
+
+__declspec(dllexport) bool __cdecl OrbitHookApi(const char* function_name, const char* module,
+                                                const char* name_space) {
+  return false;
+}
+
+}
