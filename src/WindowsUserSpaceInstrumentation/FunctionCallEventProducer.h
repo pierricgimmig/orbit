354c61e5e3f48eabf2181fd9016ff11c73d0abee
Pierric Gimmig <pierric.gimmig@gmail.com>
2023-02-06 07:35:23 -0800

    FunctionCallEventProducer instead of FunctionEntryExit

---
+++ src/WindowsUserSpaceInstrumentation/FunctionCallEventProducer.h
@@ -0,0 +1,48 @@
+// Copyright (c) 2023 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef WINDOWS_USER_SPACE_INSTRUMENTATION_FUNCTION_CALL_EVENT_PRODUCER_H_
+#define WINDOWS_USER_SPACE_INSTRUMENTATION_FUNCTION_CALL_EVENT_PRODUCER_H_
+
+#include <variant>
+
+#include "CaptureEventProducer/LockFreeBufferCaptureEventProducer.h"
+#include "GrpcProtos/instrumentation.pb.h"
+#include "ProducerSideChannel/ProducerSideChannel.h"
+
+struct FunctionCall {
+  uint32_t pid;
+  uint32_t tid;
+  uint64_t function_id;
+  uint64_t begin_timestamp_ns;
+  uint64_t end_timestamp_ns;
+};
+
+// This class is used to enqueue FunctionEntry and FunctionExit events from multiple threads,
+// transform them into orbit_grpc_protos::FunctionCall protos, and relay them to OrbitService.
+class FunctionCallEventProducer
+    : public orbit_capture_event_producer::LockFreeBufferCaptureEventProducer<FunctionCall> {
+ public:
+  FunctionCallEventProducer() {
+    BuildAndStart(orbit_producer_side_channel::CreateProducerSideChannel());
+  }
+
+  ~FunctionCallEventProducer() override { ShutdownAndWait(); }
+
+ protected:
+  [[nodiscard]] orbit_grpc_protos::ProducerCaptureEvent* TranslateIntermediateEvent(
+      FunctionCall&& raw_event, google::protobuf::Arena* arena) override {
+    auto* capture_event =
+        google::protobuf::Arena::CreateMessage<orbit_grpc_protos::ProducerCaptureEvent>(arena);
+    orbit_grpc_protos::FunctionCall* function_call = capture_event->mutable_function_call();
+    function_call->set_pid(raw_event.pid);
+    function_call->set_tid(raw_event.tid);
+    function_call->set_function_id(raw_event.function_id);
+    function_call->set_duration_ns(raw_event.end_timestamp_ns - raw_event.begin_timestamp_ns);
+    function_call->set_end_timestamp_ns(raw_event.end_timestamp_ns);
+    return capture_event;
+  }
+};
+
+#endif  // WINDOWS_USER_SPACE_INSTRUMENTATION_FUNCTION_CALL_EVENT_PRODUCER_H_
